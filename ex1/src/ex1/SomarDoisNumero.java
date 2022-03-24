24abc54daaba0bb30e432fafa5c8344ffc2f448e
andre <andre@192.168.2.6>
2022-03-24 20:53:07 -0300

    f

--- ex1/src/ex1/SomarDoisNumero.java
+++ ex1/src/ex1/SomarDoisNumero.java
@@ -1,14 +1,22 @@
 package ex1;
+import java.util.*;
+class SomarDoisNumero 
+{
+	public static Scanner sc = new Scanner(System.in);
 
-public class SomaNumero {
-
-	public static void main(String[] args) 
+	public static void main (String args[])
 	{
-		int x = 4;
-		int y = 3;
-		int sum;
-		sum = x + y;
-		System.out.println(sum);
-	}
+		//Declaracao de variaveis
+		int num1, num2, soma;
+		//Leituras
+		System.out.println("Digite um número");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro número");
+		num2 = sc.nextInt();
+		//Somar
+		soma =  num1 +  num2;
+		//Mostrar na tela
+		System.out.println("Soma:" + soma);          
+		}
 
 }
