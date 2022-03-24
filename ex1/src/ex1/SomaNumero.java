72d8463912c7d81080ea276208e336fa2b5c8556
andre <andre@192.168.2.6>
2022-03-24 18:09:44 -0300

    Ex01 ti2

---
+++ ex1/src/ex1/SomaNumero.java
@@ -0,0 +1,14 @@
+package ex1;
+
+public class SomaNumero {
+
+	public static void main(String[] args) 
+	{
+		int x = 4;
+		int y = 3;
+		int sum;
+		sum = x + y;
+		System.out.println(sum);
+	}
+
+}
